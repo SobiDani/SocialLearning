e268d801fcf3259819da05ffa8867395006f51bd
adrians_diaz <asanchez@ya-voz.com>
2022-07-06 16:29:22 +0200

    ESQUELETO NODE UP

---
+++ Back/src/api/models/herramientas.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const HerramientaSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    type: { type: String, required: true },
+    description: { type: String, required: false },
+    hability: { type: String, required: false },
+    ico: { type: String, required: true}
+  },
+  { timestamps: true }
+);
+
+const Herramienta = mongoose.model("Herramientas", HerramientaSchema);
+
+module.exports = Herramienta;
