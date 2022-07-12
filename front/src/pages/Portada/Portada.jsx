5094e87e80f5410b8a5822a431200dbd10fb6b9b
adrians_diaz <asanchez@ya-voz.com>
2022-07-13 01:15:34 +0200

    portada que gira

--- front/src/pages/Portada/Portada.jsx
+++ front/src/pages/Portada/Portada.jsx
@@ -7,7 +7,7 @@ const Portada = () => {
   return (
     <div className='PortadaBack'>
         <img className="img1Portada rotate" src={image} alt="asd"  />
-        <img className="img2Portada" src={image2} alt="asd" />
+        <img className="img2Portada rotate" src={image2} alt="asd" />
         <img className="img3Portada" src={image3} alt="asd" />
     </div>
   )
